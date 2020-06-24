1f67d1a4319824c35f8225dfa54e3e5988263150
Yurii Z <akage.gd@gmail.com>
2020-06-24 16:45:03 +0300

    + add StraightAlgorithm

--- DurationScore/TimeCalculation/Handler/StraightAlgorithmHandler.php
+++ DurationScore/TimeCalculation/Handler/StraightAlgorithmHandler.php
@@ -5,10 +5,22 @@ namespace ZYS\DurationScoreBundle\DurationScore\TimeCalculation\Handler;
 
 
 use ZYS\DurationScoreBundle\DurationScore\TimeCalculation\TimeCalculationInterface;
+use ZYS\DurationScoreBundle\Service\StraightDistance;
 
 class StraightAlgorithmHandler implements TimeCalculationInterface
 {
     const STRAIGHT = 'straight';
+    const SPEED    = 30;
+
+    /**
+     * @var StraightDistance
+     */
+    private $straightDistance;
+
+    public function __construct(StraightDistance $straightDistance)
+    {
+        $this->straightDistance = $straightDistance;
+    }
 
     public function canHandle(string $algorithm): bool
     {
@@ -17,6 +29,10 @@ class StraightAlgorithmHandler implements TimeCalculationInterface
 
     public function calculate(string $homeCoordinates, string $workCoordinates): int
     {
-        // TODO: Implement calculate() method.
+        [$lat1, $lon1] = explode(',', $homeCoordinates);
+        [$lat2, $lon2] = explode(',', $workCoordinates);
+        $distance = $this->straightDistance->distance($lat1, $lon1, $lat2, $lon2);
+
+        return round($distance / static::SPEED);
     }
 }
