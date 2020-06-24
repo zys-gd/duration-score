80b75065624ccd200f6a3a7335f5695faf94b09f
Yurii Z <akage.gd@gmail.com>
2020-06-24 16:10:14 +0300

    * update google handlers

--- DurationScore/TimeCalculation/Handler/GoogleDirections30Handler.php
+++ DurationScore/TimeCalculation/Handler/GoogleDirections30Handler.php
@@ -5,10 +5,34 @@ namespace ZYS\DurationScoreBundle\DurationScore\TimeCalculation\Handler;
 
 
 use ZYS\DurationScoreBundle\DurationScore\TimeCalculation\TimeCalculationInterface;
+use ZYS\DurationScoreBundle\Service\Google\DistanceMatrix;
+use ZYS\DurationScoreBundle\Service\Google\DistanceSimplifier;
 
 class GoogleDirections30Handler implements TimeCalculationInterface
 {
     const GOOGLE_DIRECTIONS_30 = 'google_directions_30';
+    const SPEED                = 30;
+
+    /**
+     * @var DistanceMatrix
+     */
+    private $distanceMatrix;
+    /**
+     * @var DistanceSimplifier
+     */
+    private $distanceSimplifier;
+
+    /**
+     * GoogleDirectionsHandler constructor.
+     *
+     * @param DistanceMatrix     $distanceMatrix
+     * @param DistanceSimplifier $distanceSimplifier
+     */
+    public function __construct(DistanceMatrix $distanceMatrix, DistanceSimplifier $distanceSimplifier)
+    {
+        $this->distanceMatrix     = $distanceMatrix;
+        $this->distanceSimplifier = $distanceSimplifier;
+    }
 
     public function canHandle(string $algorithm): bool
     {
@@ -17,6 +41,9 @@ class GoogleDirections30Handler implements TimeCalculationInterface
 
     public function calculate(string $homeCoordinates, string $workCoordinates): int
     {
-        // TODO: Implement calculate() method.
+        $distanceMatrix = $this->distanceMatrix->getDistance($homeCoordinates, $workCoordinates);
+        $data           = $this->distanceSimplifier->convert($distanceMatrix);
+
+        return round($data['distance'] / static::SPEED);
     }
 }
--- DurationScore/TimeCalculation/Handler/GoogleDirectionsHandler.php
+++ DurationScore/TimeCalculation/Handler/GoogleDirectionsHandler.php
@@ -5,10 +5,33 @@ namespace ZYS\DurationScoreBundle\DurationScore\TimeCalculation\Handler;
 
 
 use ZYS\DurationScoreBundle\DurationScore\TimeCalculation\TimeCalculationInterface;
+use ZYS\DurationScoreBundle\Service\Google\DistanceMatrix;
+use ZYS\DurationScoreBundle\Service\Google\DistanceSimplifier;
 
 class GoogleDirectionsHandler implements TimeCalculationInterface
 {
     const GOOGLE_DIRECTIONS = 'google_directions';
+
+    /**
+     * @var DistanceMatrix
+     */
+    private $distanceMatrix;
+    /**
+     * @var DistanceSimplifier
+     */
+    private $distanceSimplifier;
+
+    /**
+     * GoogleDirectionsHandler constructor.
+     *
+     * @param DistanceMatrix     $distanceMatrix
+     * @param DistanceSimplifier $distanceSimplifier
+     */
+    public function __construct(DistanceMatrix $distanceMatrix, DistanceSimplifier $distanceSimplifier)
+    {
+        $this->distanceMatrix     = $distanceMatrix;
+        $this->distanceSimplifier = $distanceSimplifier;
+    }
 
     public function canHandle(string $algorithm): bool
     {
@@ -17,6 +40,9 @@ class GoogleDirectionsHandler implements TimeCalculationInterface
 
     public function calculate(string $homeCoordinates, string $workCoordinates): int
     {
-        // TODO: Implement calculate() method.
+        $distanceMatrix = $this->distanceMatrix->getDistance($homeCoordinates, $workCoordinates);
+        $data           = $this->distanceSimplifier->convert($distanceMatrix);
+
+        return round($data['time']);
     }
 }
