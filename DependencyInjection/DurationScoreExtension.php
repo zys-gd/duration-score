34b307048fe8844d7aefaceb9abf0f701b83588d
Yurii Z <akage.gd@gmail.com>
2020-06-24 09:36:21 +0300

    * fix di

--- DependencyInjection/DurationScoreExtension.php
+++ DependencyInjection/DurationScoreExtension.php
@@ -3,21 +3,19 @@
 namespace ZYS\DurationScoreBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class DurationScoreExtension extends ConfigurableExtension
+class DurationScoreExtension extends Extension
 {
-
-    protected function loadInternal(array $mergedConfig, ContainerBuilder $container)
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
-
-        // $service = $container->getDefinition('cross_subscription.api_connector');
-        // $service->replaceArgument(1, $mergedConfig['api_endpoint']);
     }
 }
